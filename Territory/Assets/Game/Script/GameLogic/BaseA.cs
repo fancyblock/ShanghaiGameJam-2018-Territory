7b0f4b1bdf40986ee5fe5f58183768e06d864ccb
fancyblock <xujia1985@gmail.com>
2018-07-08 10:31:22 +0800

    hybrid

--- Territory/Assets/Game/Script/GameLogic/BaseA.cs
+++ Territory/Assets/Game/Script/GameLogic/BaseA.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 public class BaseA : MonoBehaviour
 {
     public TextMesh txtIncome;
+    public GameObject flagMake;
+    public GameObject incomeObj;
 
 
     private void Start()
     {
         onOccupyChanged();
 
+        incomeObj.SetActive(false);
+        flagMake.SetActive(true);
+
         OccupyChangeSignal signal =(GameRoot.Instance.context as GameContext).injectionBinder.GetInstance<OccupyChangeSignal>();
         signal.AddListener(onOccupyChanged);
+
+        MapRefreshSignal signal2 = (GameRoot.Instance.context as GameContext).injectionBinder.GetInstance<MapRefreshSignal>();
+        signal2.AddListener(onPlayerTurnStart);
+    }
+
+    private void onPlayerTurnStart()
+    {
+        incomeObj.SetActive(true);
+        flagMake.SetActive(false);
+
+        StartCoroutine(showIncome());
+    }
+
+    private IEnumerator showIncome()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        incomeObj.SetActive(false);
+
+        yield return new WaitForSeconds(0.2f);
+        flagMake.SetActive(true);
     }
 
     private void onOccupyChanged()
